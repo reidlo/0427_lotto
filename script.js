c0e5291147ad7d28899f1071635f855fb811f4c8
reidlo <naru7922@gmail.com>
2021-04-27 17:07:16 +0900

    0427 최종수정

---
+++ script.js
@@ -0,0 +1,14 @@
+var lotto = [];
+        while(lotto.length<6){
+        var num = lotto.push(parseInt(Math.random()*45+1));
+        if(lotto.indexOf(num) == -1) {
+            lotto.push(num);
+        }
+    }
+        lotto.sort((a,b)=>a-b);
+        document.write("<div class ='ball ball1'>" + lotto[0] + "</div>");
+        document.write("<div class ='ball ball2'>" + lotto[1] + "</div>");
+        document.write("<div class ='ball ball3'>" + lotto[2] + "</div>");
+        document.write("<div class ='ball ball4'>" + lotto[3] + "</div>");
+        document.write("<div class ='ball ball5'>" + lotto[4] + "</div>");
+        document.write("<div class ='ball ball6'>" + lotto[5] + "</div>");
